631616d58e59c13866e5a96f5650d060f9f31a36
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2025-01-16 11:44:23 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ Regional Grid Expansion Model in Germany
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "data/powerplant.png"
+logo = "data/grid.png"
 st.sidebar.image(logo)
 
 # Customize page title
