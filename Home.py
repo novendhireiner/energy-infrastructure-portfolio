2010ee107feac0a058b3095e3074bd4faa433858
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2025-01-21 12:00:00 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -51,10 +51,10 @@ This portfolio showcases **real-world case studies, interactive visualizations, 
 st.write("### 📬 Let's Connect!")
 
 st.markdown("""
-📧 **Email**: [novendhi.reiner@gmail.com](mailto:novendhi.reiner@gmail.com)  
 🔗 **GitHub**: [novendhireiner](https://github.com/novendhireiner)  
 💼 **LinkedIn**: [LinkedIn Profile](https://www.linkedin.com/in/nrmaturbongs)  
-🎓 **Certifications**: [Scrum Master Certification](https://scrum.org/certificates/1089070)  
+🎓 **Certifications**: [Scrum Master Certification](https://scrum.org/certificates/1089070)
+📧 **Email**: [novendhi.reiner@gmail.com](mailto:novendhi.reiner@gmail.com)  
 """)
 
 st.write("Feel free to reach out to discuss data analytics, energy infrastructure, or potential collaborations! 🚀")
