127f00955fd459ef9009a47ff6ecae113b2919d9
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2025-02-03 09:12:37 +0700

    range limit edit sensitivity analysis

--- pages/2_📈_Capacity_Expansion.py
+++ pages/2_📈_Capacity_Expansion.py
@@ -284,6 +284,8 @@ if st.sidebar.button("Optimize System"):
 
     fig = px.area(df, x=df.index, y=df.columns, title="System Cost vs CO₂ Emissions",
                   labels={"index": "CO₂ Emissions (MtCO₂)", "value": "System Cost (bn€/a)"},
-                 color=df.columns.map(n.carriers.color), color_discrete_map=color_mapping, xlim=(0, 150), ylim=(0, 100))
-    fig.update_layout(xaxis_title="CO₂ Emissions (MtCO₂)", yaxis_title="System Cost (bn€/a)")
+                 color=df.columns.map(n.carriers.color), color_discrete_map=color_mapping)
+    fig.update_xaxes(range=[0, 150], title="CO₂ Emissions (MtCO₂)")
+    fig.update_yaxes(range=[0, 100], title="System Cost (bn€/a)")
+    
     st.plotly_chart(fig, use_container_width=True)
